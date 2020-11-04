2bf661a7c33c4ed109f779e071cdbb875d6b340c
siminarosioru <simina.rosioru@gmail.com>
2020-11-04 15:38:42 +0200

    Changes

--- SQLServer_DataGridView_CSharp/Form1.Designer.cs
+++ SQLServer_DataGridView_CSharp/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonUpdateRecord = new System.Windows.Forms.Button();
+            this.buttonShowRecords = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,11 +45,33 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // buttonUpdateRecord
+            // 
+            this.buttonUpdateRecord.Location = new System.Drawing.Point(506, 12);
+            this.buttonUpdateRecord.Name = "buttonUpdateRecord";
+            this.buttonUpdateRecord.Size = new System.Drawing.Size(159, 56);
+            this.buttonUpdateRecord.TabIndex = 1;
+            this.buttonUpdateRecord.Text = "UPDATE RECORD";
+            this.buttonUpdateRecord.UseVisualStyleBackColor = true;
+            this.buttonUpdateRecord.Click += new System.EventHandler(this.buttonUpdateRecord_Click);
+            // 
+            // buttonShowRecords
+            // 
+            this.buttonShowRecords.Location = new System.Drawing.Point(321, 12);
+            this.buttonShowRecords.Name = "buttonShowRecords";
+            this.buttonShowRecords.Size = new System.Drawing.Size(159, 56);
+            this.buttonShowRecords.TabIndex = 2;
+            this.buttonShowRecords.Text = "SHOW RECORDS";
+            this.buttonShowRecords.UseVisualStyleBackColor = true;
+            this.buttonShowRecords.Click += new System.EventHandler(this.buttonShowRecords_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(710, 418);
+            this.Controls.Add(this.buttonShowRecords);
+            this.Controls.Add(this.buttonUpdateRecord);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -60,6 +84,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonUpdateRecord;
+        private System.Windows.Forms.Button buttonShowRecords;
     }
 }
 
--- SQLServer_DataGridView_CSharp/Form1.cs
+++ SQLServer_DataGridView_CSharp/Form1.cs
@@ -15,39 +15,34 @@ namespace SQLServer_DataGridView_CSharp
 {
     public partial class Form1 : Form
     {
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dt;
+
         public Form1()
         {
             InitializeComponent();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = GetPersonsList();
         }
-
-        private DataTable GetPersonsList()
-        {
-            DataTable dtPersons = new DataTable();
-
-            string conString = ConfigurationManager.ConnectionStrings["dbx"].ConnectionString;
-
-
-            using (SqlConnection objConnection = new SqlConnection(conString))
-            {
-                using (SqlCommand objCommand = new SqlCommand("SELECT * FROM Table_Users", objConnection))
-                {
-                    objConnection.Open();
-
-                    SqlDataReader reader = objCommand.ExecuteReader();
-
-                    dtPersons.Load(reader);
-                }
-            }
-
-            return dtPersons;
-        }
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+        }
+
+        private void buttonShowRecords_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-0U8N3MS\\MSSQLSERVER2020;Initial Catalog=MagazinDb;Integrated Security=True");
+            sda = new SqlDataAdapter("SELECT * FROM Table_Users", connection);
+            dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void buttonUpdateRecord_Click(object sender, EventArgs e)
+        {
+            scb = new SqlCommandBuilder(sda);
+            sda.Update(dt);
         }
 
     }

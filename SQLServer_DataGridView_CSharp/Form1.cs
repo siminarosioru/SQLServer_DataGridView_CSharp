410c2e82cbe06d4a0a492813d25f27fe18ef8254
siminarosioru <simina.rosioru@gmail.com>
2020-11-05 09:06:27 +0200

    add commits

--- SQLServer_DataGridView_CSharp/Form1.cs
+++ SQLServer_DataGridView_CSharp/Form1.cs
@@ -15,12 +15,14 @@ namespace SQLServer_DataGridView_CSharp
 {
     public partial class Form1 : Form
     {
+        //declare the classes + the names of the objects
         SqlDataAdapter sda;
         SqlCommandBuilder scb;
         DataTable dt;
 
         public Form1()
         {
+            //constructor
             InitializeComponent();
         }
         private void Form1_Load(object sender, EventArgs e)
@@ -32,16 +34,30 @@ namespace SQLServer_DataGridView_CSharp
 
         private void buttonShowRecords_Click(object sender, EventArgs e)
         {
+            //define the connection string
             SqlConnection connection = new SqlConnection("Data Source=DESKTOP-0U8N3MS\\MSSQLSERVER2020;Initial Catalog=MagazinDb;Integrated Security=True");
+
+            //the object instantiates the SqlDataAdapter class with arguments
+            //specify which table (with the primary key) you will use as the first argument 
+            //and the second argument is the connection string
             sda = new SqlDataAdapter("SELECT * FROM Table_Users", connection);
+
+            //the object instantiates the DataTable class without arguments
             dt = new DataTable();
+
+            //fill the data table
             sda.Fill(dt);
+
+            //set the data source -> the dt object returns a data table
             dataGridView1.DataSource = dt;
         }
 
         private void buttonUpdateRecord_Click(object sender, EventArgs e)
         {
+            //the object instantiates the SqlCommandBuilder class with argument(the sda object)
             scb = new SqlCommandBuilder(sda);
+
+            //update the values in the database
             sda.Update(dt);
         }
 
